a211746358758e4f789c48c0f549b1e944cd0526
martinbasmad <ombasmadjian@gmail.com>
2025-02-07 17:47:40 -0300

    Delete src/.ipynb_checkpoints directory
    e

--- src/.ipynb_checkpoints/main-checkpoint.py
+++
@@ -1,6 +0,0 @@
-from modules import subtitle_generator
-
-if __name__ == "__main__":
-    video_url = input("🔗 Ingresa la URL del video de YouTube: ").strip()
-    generator = subtitle_generator.SubtitleGenerator(video_url)
-    generator.run()
